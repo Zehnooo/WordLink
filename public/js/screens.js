ed12324db3d974ee7969d3a8d6b2b9dd0bfbfaae
Zehno <126801501+Zehnooo@users.noreply.github.com>
2026-09-25 12:05:50 -0500

    added error handling on change screen and updated naming in html

--- public/js/screens.js
+++ public/js/screens.js
@@ -3,6 +3,7 @@ import { chainListView, errorView } from "./dom.js";
 export const changeScreen = (screen) => {
     document.querySelectorAll('.screen').forEach(screen => screen.setAttribute("hidden", "hidden"));
     const screenEl = document.querySelector(`#${screen}`);
+    if (!screenEl) { console.error(`Unknown screen: ${screen}`); return; }
     showContent(screenEl.id);
     screenEl.removeAttribute("hidden");
 }
