3b1ee066e598bef3cc33fced1fcca8582f96c5c9
Zehno <126801501+Zehnooo@users.noreply.github.com>
2026-09-25 12:12:39 -0500

    added more styles to existing chainListView elements

--- public/js/dom.js
+++ public/js/dom.js
@@ -4,7 +4,8 @@ import icons from './icons.js';
 export const chainListView = () => {
 
     const mainCon = newEl('div', null, 'chain-list-content', ['view-content']);
-    const topBar = newEl('div', null, 'chain-list-actions', ['fx']);
+    const topBar = newEl('div', null, 'chain-list-main', ['fx', 'fdc', 'w']);
+    const newCon = newEl('div', null, null, ['fx', 'w']);
 
     const title = newEl('h2', 'Chain Lists');
 
@@ -12,14 +13,17 @@ export const chainListView = () => {
     newListBtn.addEventListener('click', () => { console.log('New List Button'); });
     newListBtn.innerHTML = icons.buttons.plus + 'New List';
 
-    const searchCon = newEl('div', null, 'chain-list-search-con', ['fx', 'ac']);
+    const searchCon = newEl('div', null, 'chain-list-search-con', ['fx', 'ac', 'w']);
     const search = newEl('input', null, 'chain-list-search-input');
-    search.addEventListener('input', (e) => { console.log(e.target.value) });
+        search.type = 'text';
+        search.placeholder = 'Search lists...';
+        search.addEventListener('input', (e) => { console.log(e.target.value) });
     const searchIcon = newEl('span');
         searchIcon.innerHTML = icons.misc.search;
 
+    newCon.append(title, newListBtn);
     searchCon.append(search, searchIcon);
-    topBar.append(title, newListBtn, searchCon);
+    topBar.append(newCon, searchCon);
     mainCon.append(topBar);
     return mainCon;
 }
