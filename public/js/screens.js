df27f7fbb6d90b18cf7972fe0afe5e097fca95c0
Zehno <126801501+Zehnooo@users.noreply.github.com>
2026-09-25 15:19:23 -0500

    added live table population after list page is loaded

--- public/js/screens.js
+++ public/js/screens.js
@@ -1,24 +1,34 @@
-import { chainListView, errorView } from "./dom.js";
+import { errorView } from "./dom.js";
+import { chainListView, populateChainListTable } from './chainListDom.js';
 
-export const changeScreen = (screen) => {
+export const changeScreen = async (screen) => {
     const screenEl = document.querySelector(`#${screen}`);
     if (!screenEl) { console.error(`Unknown screen: ${screen}`); return; }
 
     document.querySelectorAll('.screen').forEach(screen => screen.setAttribute("hidden", "hidden"));
-    showContent(screenEl.id);
+    await showContent(screenEl.id);
     screenEl.removeAttribute("hidden");
 }
 
-const showContent = (screenId) => {
+const showContent = async (screenId) => {
     const contentDiv = document.querySelector(`#${screenId}-content`);
-    if (!contentDiv) { console.error(`Content Div for ${screenId} was not found.`); return; }
+    if (!contentDiv) { return { success: false, message: `Content Div for ${screenId} was not found.`}; }
     const render = screenContent[screenId] ?? screenContent['error-screen'];
     contentDiv.replaceChildren(render());
+    if (Object.hasOwn(loadContent, screenId)) {
+        const load = await loadContent[screenId]();
+        if (!load.success){ console.error(load.message); }
+        document.querySelector('#placeholder').remove();
+    }
 }
 
 const screenContent = {
-    //'home-screen': homeView();
+    //'home-screen': homeView,
     'chain-list-screen': chainListView,
     'error-screen': errorView,
+}
+
+const loadContent = {
+    'chain-list-screen': populateChainListTable,
 }
 
